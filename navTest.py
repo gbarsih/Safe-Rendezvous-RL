81d8832a8ada76d62239789716d40a21e5fc62df
Gabriel Barsi Haberfeld <gbh2@illinois.edu>
2021-05-07 20:19:12 -0500

    updating images and routing mechanism

--- navTest.py
+++ navTest.py
@@ -6,6 +6,7 @@ import torchvision.transforms as transforms
 import numpy as np
 import multiprocessing as mp
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 import math
 import geopy.distance as gd
 
@@ -13,6 +14,9 @@ import osmnx as ox
 import networkx as nx
 import importlib as im
 import utils
+import random
+
+im.reload(utils)
 
 coords_1 = (52.2296756, 21.0122287)
 coords_2 = (52.406374, 16.9251681)
@@ -21,66 +25,125 @@ print(gd.distance(coords_1, coords_2).km)
 
 cpus = mp.cpu_count() - 1
 ox.config(use_cache=True, log_console=True)
-sim_env_dt = 1 #envinronment updates every 1s
+sim_env_dt = 1  # envinronment updates every 1s
 EARTH_RADIUS_M = 6_371_009
 
-
 # filepathuc = "./data/data.graphml"
-# G, gdf, gdf_water, gdf_parks = getUcMap()
-
-filepathcc = "./data/data_cc.graphml"
-G, gdf, gdf_water, gdf_parks = utils.getChampCountyMap(filepathcc)
+# G, gdf, gdf_water, gdf_parks = utils.getUCMap()
+places = ['Champaign, Illinois, USA', 'Urbana, Illinois, USA']
+# places = 'Chicago, Illinois, USA'
+G = ox.graph_from_place(places, network_type="drive",simplify=False)
+G = ox.add_edge_speeds(G)
+G = ox.add_edge_travel_times(G)
+G = ox.bearing.add_edge_bearings(G)
+# filepathcc = "./data/data_cc.graphml"
+# G, gdf, gdf_water, gdf_parks = utils.getChampCountyMap(filepathcc)
 
 bbox = (40.1213, 40.0996, -88.1981, -88.2455)
 
 routex = np.random.uniform(low=bbox[0], high=bbox[1], size=2)
 routey = np.random.uniform(low=bbox[2], high=bbox[3], size=2)
 
-orig = ox.get_nearest_node(G, (routex[0], routey[0]))
-dest = ox.get_nearest_node(G, (routex[1], routey[1]))
+nroutes = 30
 
-route1 = ox.shortest_path(G, orig, dest, weight="length")
-route2 = ox.shortest_path(G, orig, dest, weight="travel_time")
+orig = random.sample(list(G), nroutes)
+dest = random.sample(list(G), nroutes)
 
-fig, ax = ox.plot_footprints(gdf, alpha=0.5, show=False, close=False, bgcolor="#ffffff", bbox=bbox)
-fig, ax = ox.plot_footprints(gdf_parks, ax=ax, alpha=0.5, color="green", show=False, bgcolor="#ffffff", bbox=bbox)
-fig, ax = ox.plot_footprints(gdf_water, ax=ax, alpha=0.5, color="blue", show=False, bgcolor="#ffffff")
-fig, ax = ox.plot_graph(G, ax=ax, node_size=1, node_color="#a3a3a3", edge_color="#a3a3a3", edge_linewidth=0.5,
-                        bgcolor="#ffffff", show=False, bbox=bbox)
+route = []
+for i in range(len(orig)):
+    try:
+        r = ox.shortest_path(G, orig[i], dest[i], weight="travel_time")
+        route.append(r)
+    except Exception as ex:
+        print(ex)
+
+# fig, ax = ox.plot_footprints(gdf, alpha=0.5, show=False, close=False, bgcolor="#ffffff")
+# fig, ax = ox.plot_footprints(gdf_parks, ax=ax, alpha=0.5, color="green", show=False, bgcolor="#ffffff")
+# fig, ax = ox.plot_footprints(gdf_water, ax=ax, alpha=0.5, color="blue", show=False, bgcolor="#ffffff")
+fig, ax = ox.plot_graph(G, node_size=1, node_color="#a3a3a3", edge_color="#a3a3a3", edge_linewidth=0.5,
+                        bgcolor="#ffffff", show=False)
 
 fig, ax = ox.plot_graph_routes(
-    G, ax=ax, routes=[route1, route2], route_colors=["r", "y"], route_linewidth=6, node_size=0, save=True, dpi=3000,
-    bbox=bbox, close=False, show=False
+    G, ax=ax, routes=route, route_colors=ox.plot.get_colors(len(route)), route_linewidth=3, node_size=0, save=True,
+    dpi=600, close=False, show=False
 )
 
 plt.show()
 
+route = ox.k_shortest_paths(G,orig[5],dest[5],5,weight="length")
+route = list(route)
+
+fig, ax = ox.plot_graph(G, node_size=1, node_color="#a3a3a3", edge_color="#a3a3a3", edge_linewidth=0.5,
+                        bgcolor="#ffffff", show=False, dpi = 3000,figsize=(20,20))
+
+fig, ax = ox.plot_graph_routes(
+    G, ax=ax, routes=route, route_colors=ox.plot.get_colors(len(route)), route_linewidth=3, node_size=0,
+    close=False, show=False, dpi = 1000,figsize=(20,20)
+); plt.show()
+
+
+
+places = ['Champaign, Illinois, USA', 'Urbana, Illinois, USA']
+places = 'Chicago, Illinois, USA'       # orig[0]
+                                        # Out[177]: 5891694350
+                                        # dest[0]
+                                        # Out[178]: 2844260418
+G = ox.graph_from_place(places, network_type="drive",simplify=False)
+G = ox.add_edge_speeds(G)
+G = ox.add_edge_travel_times(G)
+G = ox.bearing.add_edge_bearings(G)
+
+nroutes = 30
+
+orig = random.sample(list(G), nroutes)
+dest = random.sample(list(G), nroutes)
+
+route1 = ox.shortest_path(G, orig[0], dest[0], weight="travel_time")
+im.reload(utils)
+x, y, s, t, d = utils.computeIntervals(G, route1, 0.0);
+r = utils.iRoute(x, y, s, t, d, 0, 0, route1)
+
+i = 0
+
+while not r.completed and i < 100000:
+    print(r.completed)
+    i += 1
+    r.step()
+    r.printDeg(G)
+
+
+print(r.local_time)
+
+
 im.reload(utils)
 
-x, y, s, v, t, n, d = utils.computeIntervals(G,route2,0.0);
-droute2 = utils.iRoute(x,y,s,v,t,n,d,(0,0))
-print(droute2.n)
+node_list = []
 
-cm = droute2.n
-cm.append(0)
-
-plt.figure(figsize=(10, 10), dpi=80)
-plt.scatter(droute2.x,droute2.y,c=cm,s=100)
-#plt.gca().set_aspect('equal', adjustable='box');
-#plt.savefig('images/raw_route.png')
-plt.gray()
-plt.show()
-
-plt.plot([x / max(droute2.d) for x in droute2.d])
-plt.plot([x / max(droute2.t) for x in droute2.t])
-plt.show()
-
-tr = np.ones(len(droute2.t))
-
-for i in range(len(tr)):
-    tr[i] = droute2.t[i] + np.random.randn(1)*np.sqrt(droute2.t[i])/5
+for idx in range(100,len(route1)-1):
+    node_list, an = utils.OneDegSep(G, route1[idx], route1[idx - 1], route1[idx+1])
+    if len(node_list) > 1: break
 
 
-plt.plot(droute2.t)
-plt.plot(tr)
+newRoute = utils.reRoute(G,route1[idx],node_list[0],dest[0])
+newAlt = utils.reRoute(G,route1[idx],node_list[1],dest[0])
+
+for idxn in range(5,len(newRoute)-1):
+    node_listn, ann = utils.OneDegSep(G, newRoute[idxn], newRoute[idxn - 1], newRoute[idxn+1])
+    if len(node_listn) > 1: break
+
+newNewRoute = utils.reRoute(G,newRoute[idxn],node_listn[0],dest[0])
+routes = [route1, newRoute, newAlt, newNewRoute]
+
+bbox = ox.utils_geo.bbox_from_point((G.nodes[newRoute[0]]['y'], G.nodes[newRoute[0]]['x']), dist=500)
+
+fig, ax = ox.plot_graph(G, node_size=50, node_color="#a3a3a3", edge_color="#a3a3a3", edge_linewidth=2,
+                        bgcolor="#ffffff", show=False, dpi = 600,figsize=(20,20), bbox=bbox);
+
+fig, ax = ox.plot_graph_routes(
+    G, ax=ax, routes=routes, route_colors=ox.plot.get_colors(len(routes)), route_linewidth=10,
+    close=False, show=False, save=True, filepath='images/reroute_consequences.png');
+ax = utils.scatterGraph(node_list,G,ax)
+ax.scatter(G.nodes[route1[idx]]['x'],G.nodes[route1[idx]]['y'],c='red',s=30)
+ax.scatter(G.nodes[route1[idx+1]]['x'],G.nodes[route1[idx+1]]['y'],c='blue',s=30)
+
 plt.show()
--- utils.py
+++ utils.py
@@ -18,31 +18,31 @@ sim_env_dt = 0.1  # envinronment updates every 1s
 EARTH_RADIUS_M = 6_371_009
 
 
-def getUcMap(_filepath=None, _gdf_filepath=None):
-    ox.config(use_cache=True, log_console=True)
-    _gdf1 = ox.geometries_from_place("Urbana, Illinois, USA", {"building": True})
-    _gdf_parks1 = ox.geometries_from_place("Urbana, Illinois, USA", {"leisure": 'park'})
-    _gdf_water1 = ox.geometries_from_place("Urbana, Illinois, USA", {"water": True})
-    _gdf2 = ox.geometries_from_place("Champaign, Illinois, USA", {"building": True})
-    _gdf_parks2 = ox.geometries_from_place("Champaign, Illinois, USA", {"leisure": 'park'})
-    _gdf_water2 = ox.geometries_from_place("Champaign, Illinois, USA", {"water": True})
-    _gdf = _gdf1.append(_gdf2)
-    _gdf_water = _gdf_water1.append(_gdf_water2)
-    _gdf_parks = _gdf_parks1.append(_gdf_parks2)
-    if _filepath is None:
-        _G1 = ox.graph_from_place("Urbana, Illinois, USA", network_type="drive")
-        _G2 = ox.graph_from_place("Champaign, Illinois, USA", network_type="drive")
-        _G = nx.compose(_G1, _G2)
-        print("Saving to file!!")
-        ox.save_graphml(_G, filepathuc)
-        return _G, _gdf, _gdf_water, _gdf_parks
-
-    else:
-        print("Loading graph from file!!")
-        _filepath = "./data/data.graphml"
-        _G = ox.load_graphml(filepath)
-
-        return _G, _gdf, _gdf_water, _gdf_parks
+# def getUcMap(_filepath=None, _gdf_filepath=None):
+#     ox.config(use_cache=True, log_console=True)
+#     _gdf1 = ox.geometries_from_place("Urbana, Illinois, USA", {"building": True})
+#     _gdf_parks1 = ox.geometries_from_place("Urbana, Illinois, USA", {"leisure": 'park'})
+#     _gdf_water1 = ox.geometries_from_place("Urbana, Illinois, USA", {"water": True})
+#     _gdf2 = ox.geometries_from_place("Champaign, Illinois, USA", {"building": True})
+#     _gdf_parks2 = ox.geometries_from_place("Champaign, Illinois, USA", {"leisure": 'park'})
+#     _gdf_water2 = ox.geometries_from_place("Champaign, Illinois, USA", {"water": True})
+#     _gdf = _gdf1.append(_gdf2)
+#     _gdf_water = _gdf_water1.append(_gdf_water2)
+#     _gdf_parks = _gdf_parks1.append(_gdf_parks2)
+#     if _filepath is None:
+#         _G1 = ox.graph_from_place("Urbana, Illinois, USA", network_type="drive")
+#         _G2 = ox.graph_from_place("Champaign, Illinois, USA", network_type="drive")
+#         _G = nx.compose(_G1, _G2)
+#         print("Saving to file!!")
+#         ox.save_graphml(_G, filepathuc)
+#         return _G, _gdf, _gdf_water, _gdf_parks
+#
+#     else:
+#         print("Loading graph from file!!")
+#         _filepath = "./data/data.graphml"
+#         _G = ox.load_graphml(filepath)
+#
+#         return _G, _gdf, _gdf_water, _gdf_parks
 
 
 def getChampCountyMap(_filepath=None):
@@ -51,7 +51,7 @@ def getChampCountyMap(_filepath=None):
     _gdf_parks = ox.geometries_from_place("Champaign County, Illinois, USA", {"leisure": 'park'})
     _gdf_water = ox.geometries_from_place("Champaign County, Illinois, USA", {"water": True})
     if _filepath is None:
-        _G = ox.graph_from_place("Champaign County, Illinois, USA", network_type="drive",simplify=False)
+        _G = ox.graph_from_place("Champaign County, Illinois, USA", network_type="drive", simplify=False)
         print("Saving to file!! ./data/data_cc.graphml")
         ox.save_graphml(_G, "./data/data_cc.graphml")
 
@@ -68,6 +68,31 @@ def getChampCountyMap(_filepath=None):
         # edges["highway"] = edges["highway"].astype(str)
         # edges.groupby("highway")[["length", "speed_kph", "travel_time"]].mean().round(1)
         # hwy_speeds = {"residential": 35, "secondary": 50, "tertiary": 60}
+        _G = ox.add_edge_speeds(_G)
+        _G = ox.add_edge_travel_times(_G)
+        _G = ox.bearing.add_edge_bearings(_G)
+        return _G, _gdf, _gdf_water, _gdf_parks
+
+
+def getUCMap(_filepath=None):
+    ox.config(use_cache=True, log_console=True)
+    places = ['Champaign, Illinois, USA', 'Urbana, Illinois, USA']
+    _gdf = ox.geometries_from_place(places, {"building": True})
+    _gdf_parks = ox.geometries_from_place(places, {"leisure": 'park'})
+    _gdf_water = ox.geometries_from_place(places, {"water": True})
+    if _filepath is None:
+        _G = ox.graph_from_place(places, network_type="drive", simplify=False)
+        print("Saving to file!! ./data/data_cc.graphml")
+        ox.save_graphml(_G, "./data/data_cc.graphml")
+
+        _G = ox.add_edge_speeds(_G)
+        _G = ox.add_edge_travel_times(_G)
+        _G = ox.bearing.add_edge_bearings(_G)
+        return _G, _gdf, _gdf_water, _gdf_parks
+
+    else:
+        print("Loading graph from file!!")
+        _G = ox.load_graphml(_filepath)
         _G = ox.add_edge_speeds(_G)
         _G = ox.add_edge_travel_times(_G)
         _G = ox.bearing.add_edge_bearings(_G)
@@ -97,7 +122,7 @@ def getRouteData(G, route):
             x.extend(xs)
             y.extend(ys)
             s.extend([a for a in data.values()][-3] * np.ones(len(xs)))
-            t.extend([a for a in data.values()][-2] * np.ones(len(xs)) / len(xs))
+            t.extend([a for a in data.values()][-2] * np.ones(len(xs)))
         else:
             # otherwise, the edge is a straight line from node to node
             x.extend((G.nodes[u]["x"], G.nodes[v]["x"]))
@@ -119,33 +144,61 @@ def getRouteData(G, route):
 
     return np.array(x), np.array(y), np.array(s), np.array(t), np.array(d)
 
-#a workable route object
+
+# a workable route object
 class iRoute:
-    def __init__(self, x, y, s, v, t, n, d, pos):
-        self.x = x  #position
+
+    def __init__(self, x, y, s, t, d, pos, global_t0, route):
+        self.x = x  # position
         self.y = y
-        self.s = s  #speeds
-        self.v = v  #clculated velocity
-        self.t = t  #time for transition
-        self.n = n  #simulation steps to transition
-        self.d = d  #distance to cover
-        self.pos = pos  #position (node index)
+        self.s = s  # speeds
+        # self.v = v  # calculated velocity
+        self.t = t  # time for transition
+        # self.n = n  # simulation steps to transition
+        self.d = d  # distance to cover
+        self.pos = pos  # position (node index)
+        self.global_t0 = global_t0
+        self.edge_timer = 1
+        self.local_time = 0.0
+        self.dt = sim_env_dt
+        # self.route_steps = sum(self.n)
+        self.lt = len(self.x)
+        self.completed = False
+        self.route = route
 
-    def __setattr__(self, name, value):
-        if name == 'pos':
-            if value[1] > self.n[value[0]]:
-                raise Exception(f"Cannot change value of {name}; attempted higher step than allowed.")
+    # this function progresses to the next node
+    def progress(self):
+        self.pos += 1  # advance one node
+        self.edge_timer = self.dt  # 1 if progressing to the next node is an action
+        return True
 
-        self.__dict__[name] = value
+    # this function advances one time step
+    def step(self):
+        self.edge_timer += self.dt
+        self.local_time += self.dt
+        stat = None
+        if self.edge_timer >= self.t[self.pos] and self.pos < self.lt and self.completed == False:
+            stat = self.progress()
+        elif self.pos == self.lt and self.completed == False:
+            self.completed = True
+            print("At end of route, pos=", self.pos, "length of t=", self.lt, "Completed:",self.completed)
+
+
+        print("Pos:", self.pos, "Travel Time:", self.t[self.pos], "Edge Time:", self.edge_timer, "Local Time:",
+              self.local_time)
+        return stat
+
+    def printDeg(self, G):
+        if self.pos < len(self.route):
+            print("Node deg:", G.nodes[self.route[self.pos]]["street_count"])
+
 
 
 def computeIntervals(G, route, vDev):
-
     x, y, s, t, d = getRouteData(G, route)
     s = np.array(s) / 3.6  # to m/s
-
-    plt.scatter(x, y);
-    plt.show()
+    # plt.scatter(x, y)
+    # plt.show()
     v = []
     n = []
     popi = []
@@ -154,21 +207,58 @@ def computeIntervals(G, route, vDev):
         norm = math.sqrt(distance[0] ** 2 + distance[1] ** 2)
         if norm == 0:  # points on top of each other
             popi.append(i)
-        else:
-            # faster drivers spend less time at any given stretch
-            td = t[i]
-            if vDev == None:
-                td += vDev(td)
-
-            steps = td/sim_env_dt
-            steps_rounded = int(round(steps,1))
-            print("Time in segment:", td, "Number of steps:", steps, "Rounded:", steps_rounded)
-            n.append(steps_rounded)
-            direction = [distance[0] / norm, distance[1] / norm]  # normalized direction
-            v.append([direction[0] * s[i], direction[1] * s[i]])
+        # else:
+        #     # faster drivers spend less time at any given stretch
+        #     td = t[i]
+        #     # if vDev is None:
+        #     #     td += vDev(td)
+        #
+        #     steps = td / sim_env_dt
+        #     steps_rounded = int(round(steps, 1))
+        #     # print("Time in segment:", td, "Number of steps:", steps, "Rounded:", steps_rounded)
+        #     n.append(int(np.maximum(0, steps_rounded)))
+        #     direction = [distance[0] / norm, distance[1] / norm]  # normalized direction
+        #     v.append([direction[0] * s[i], direction[1] * s[i]])
     x = np.delete(x, popi)
     y = np.delete(y, popi)
     s = np.delete(s, popi)
 
-    return x, y, s, v, t, n, d
+    return x, y, s, t, d
+
+def scatterGraph(node_list,G: nx.classes.multidigraph.MultiDiGraph, ax,color='green'):
+    for i in range(len(node_list)):
+        ax.scatter(G.nodes[node_list[i]]['x'],G.nodes[node_list[i]]['y'],c=color,s=10)
+
+    return ax
+
+def OneDegSep(G: nx.classes.multidigraph.MultiDiGraph, node, prev_node = None, next_node = None):
+    an = nx.generators.ego_graph(G, node)
+    node_list = list(an.nodes)
+    node_list.remove(node)
+    if prev_node is not None:
+        try:
+            node_list.remove(prev_node)
+        except Exception as ex:
+            print("prev node unreachable")
+
+    if next_node is not None:
+        node_list.remove(next_node)
+
+    return node_list, an
+
+def reRoute(G,prev_node,curr_node,dest):
+    #Finds a new route that doesn't go through any of the past nodes as defined in argin
+
+    try:
+        temp = G[curr_node][prev_node][0]['travel_time']
+        G[curr_node][prev_node][0]['travel_time'] = 1e6
+        newRoute = ox.shortest_path(G,curr_node,dest,weight="travel_time")
+        G[curr_node][prev_node][0]['travel_time'] = temp
+    except Exception as ex:
+        print("street is one-way, no weight assgntm")
+        newRoute = ox.shortest_path(G,curr_node,dest,weight="travel_time")
+
+    return newRoute
+
+
 
